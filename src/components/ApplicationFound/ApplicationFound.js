61debb68ab40237498ed618b95919fc9dedfe6cc
yamathe <manuelsegmar@gmail.com>
2022-09-05 09:56:35 -0500

    ADD: Company  page components

---
+++ src/components/ApplicationFound/ApplicationFound.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function ApplicationFound() {
+  return (
+    <div className="application-found-card">
+      <img />
+      <h2>The job title</h2>
+      <h3>The Company Name SA</h3>
+      <p>Manufactoring | Full time</p>
+      <p>2.0k - 2.5k | Posted 2 days ago</p>
+      <div>
+        <p>Sent 1 min. ago</p>
+      </div>
+      <div>
+        <p>Waiting for review</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/CompanyComponents/CreateNewJob/CreateNewJob.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CreateNewJob() {
+  return (
+    <>
+      <h2>Create new job posting</h2>
+      <h3>Main information</h3>
+    </>
+  );
+}
