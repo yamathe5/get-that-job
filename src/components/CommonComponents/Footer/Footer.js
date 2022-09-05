2a14a4c7696748c33625b4476696e0ac972833f9
yamathe <manuelsegmar@gmail.com>
2022-09-05 09:53:34 -0500

    ADD: Footer component

---
+++ src/components/CommonComponents/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="subtitle-1-bold">© 2021 - Get That Job</p>
+      <p className="subtitle-1-bold">
+        Source Code <br /> Ruby on Rails REST API React Responsive SPA
+      </p>
+      <p className="subtitle-1-bold">Codeable - Cohort 3 Final Project</p>
+    </footer>
+  );
+}
